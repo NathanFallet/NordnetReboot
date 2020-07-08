b062033fca1cd2e2286e8bcf8c53c989574c914a
Nathan Fallet <contact@nathanfallet.me>
2020-07-08 16:30:47 +0200

    Initial Commit

---
+++ NordnetReboot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NordnetReboot
+//
+//  Created by Nathan FALLET on 08/07/2020.
+//  Copyright © 2020 Nathan FALLET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
